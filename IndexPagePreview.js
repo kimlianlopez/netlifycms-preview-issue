88f0bef717150748025cc262bbd8cbd25d5a1c26
Kim Lian Lopez <kimlian.dev@gmail.com>
2019-11-26 10:48:46 +0800

    Initial Commit

--- IndexPagePreview.js
+++ IndexPagePreview.js
@@ -3,27 +3,27 @@ import PropTypes from 'prop-types';
 import { IndexPageTemplate } from '../../pages/index';
 
 const IndexPagePreview = ({ entry, getAsset }) => {
-	const data = entry.getIn(['data']).toJS();
+  const data = entry.getIn(['data']).toJS();
 
-	if (data) {
-		return (
-			<IndexPageTemplate
-				heroSection={data.heroSection}
-				aboutSection={data.aboutSection}
-				featuredSection={data.featuredSection}
-				contactSection={data.contactSection}
-			/>
-		);
-	} else {
-		return <div>Loading...</div>;
-	}
+  if (data) {
+    return (
+      <IndexPageTemplate
+        heroSection={data.heroSection}
+        aboutSection={data.aboutSection}
+        featuredSection={data.featuredSection}
+        contactSection={data.contactSection}
+      />
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
 };
 
 IndexPagePreview.propTypes = {
-	entry: PropTypes.shape({
-		getIn: PropTypes.func
-	}),
-	getAsset: PropTypes.func
+  entry: PropTypes.shape({
+    getIn: PropTypes.func
+  }),
+  getAsset: PropTypes.func
 };
 
 export default IndexPagePreview;
